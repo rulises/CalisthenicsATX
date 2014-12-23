7db20b235bd1d2e2014a1f433dbb7c805640cb24
Ulises <ulises.card@gmail.com>
2014-12-23 14:05:36 -0600

    mongolab

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var flash = require('connect-flash');
+var session = require('express-session');
 
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
