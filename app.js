115ca8ad34405438785e069ecf74fbec289efe67
Ulises <ulises.card@gmail.com>
2014-12-23 10:22:19 -0600

    mongolab

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require('express');
+VAR EXPRESS = REQUIRE('EXPRESS');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
