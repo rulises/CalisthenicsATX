2fe8d361bf74a33aa0bd0f37d6593bb7f0416fec
Ulises Cardenas <ulises.card@gmail.com>
2015-01-10 15:41:30 -0600

    das
    dasd

--- routes.js
+++ routes.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
+module.exports = function(app, passport){
+/* GET home page. */
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
 /* LOGIN */
 app.get('/login', function(req, res){
     res.render('login.ejs', { message:req.flash('loginMessage') });    
@@ -25,7 +32,9 @@ app.post('/signup', passport.authenticate('local-signup', {
 }));
 /* PROFILE */
 app.get('/profile',isLoggedIn, function(req, res){
-    res.render('profile.ejs', { user:req.user });    
+    res.render('profile.ejs', { 
+        user:req.user 
+    });    
 });
 
 /* LOGOUT */
@@ -33,6 +42,26 @@ app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');    
 });
+
+// =====================================
+// FACEBOOK ROUTES =====================
+// =====================================
+// route for facebook authentication and login
+app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+// handle the callback after facebook has authenticated the user
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+// route for logging out
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+};
 
 };
 
